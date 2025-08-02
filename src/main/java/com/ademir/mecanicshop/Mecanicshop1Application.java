160452c6cd1fcd1e66a6ebbb1b9c6a2fe9d870fa
MatheusGoncalves73 <matheus57.ruiz100@outlook.com>
2025-08-02 06:57:51 -0300

    Projeto Inicial

---
+++ src/main/java/com/ademir/mecanicshop/Mecanicshop1Application.java
@@ -0,0 +1,13 @@
+package com.ademir.mecanicshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mecanicshop1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mecanicshop1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ademir/mecanicshop/Mecanicshop1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ademir.mecanicshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mecanicshop1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
