94e8e578d7e820fbdae8ae0129e086b077de1f84
MatheusGoncalves73 <matheus57.ruiz100@outlook.com>
2025-08-03 05:48:09 -0300

    Add repository annotation

--- src/main/java/com/ademir/mecanicshop/repositories/CustomerRepository.java
+++ src/main/java/com/ademir/mecanicshop/repositories/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.ademir.mecanicshop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ademir.mecanicshop.domain.Customer;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 }
