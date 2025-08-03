b1c520f647f1dfcd8119e070f45efc8afa72be8d
MatheusGoncalves73 <matheus57.ruiz100@outlook.com>
2025-08-03 15:50:02 -0300

    Add Endpoint to Delete Vehicle by Id

--- src/main/java/com/ademir/mechanicshop/resources/VehicleResource.java
+++ src/main/java/com/ademir/mechanicshop/resources/VehicleResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,5 +62,11 @@ public class VehicleResource {
 	public ResponseEntity<VehicleDTO> update(@PathVariable Integer id, @RequestBody VehicleDTO objDTO){
 		Vehicle vehicle = this.vehicleService.update(id,objDTO);
 		return ResponseEntity.ok().body(new VehicleDTO(vehicle));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		this.vehicleService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/ademir/mechanicshop/services/VehicleService.java
+++ src/main/java/com/ademir/mechanicshop/services/VehicleService.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ademir.mechanicshop.domain.Vehicle;
 import com.ademir.mechanicshop.dtos.VehicleDTO;
 import com.ademir.mechanicshop.repositories.CustomerRepository;
 import com.ademir.mechanicshop.repositories.VehicleRepository;
+import com.ademir.mechanicshop.services.exceptions.DataIntegrityException;
 import com.ademir.mechanicshop.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -47,6 +49,15 @@ public class VehicleService {
 		newVehicle.setYear(obj.getYear());
 		this.vehicleRepository.save(newVehicle);
 		return newVehicle;
+	}
+
+	public void delete(Integer id) {
+		try {
+			Vehicle obj = this.findById(id);
+			this.vehicleRepository.delete(obj);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Veículo não pode ser deletado pois possui ordens de serviço associadas");
+		}
 	}
 
 }
